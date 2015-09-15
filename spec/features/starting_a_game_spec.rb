56eafd8738a83772f207ff49f7cd3b6956d70e5d
Harry Venables <venablesh@yahoo.co.uk>
2015-09-15 14:02:20 +0100

    Added name form to webpage

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,9 +6,18 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+  scenario 'Page has field to input name' do
+    visit '/new_game'
+    expect(page).to have_field('name')
+  end
+  scenario 'Submit button exists' do
+    visit '/new_game'
+    expect(page).to have_selector("input[type='submit'][value='Submit']")
+  end
   scenario 'I can input my name into form' do
-    visit '/'
-    click_link 'New Game'
-    expect(page).to have_selector('input[type=submit]')
+    visit '/new_game'
+    fill_in 'name', with: 'Harry'
+    click_button 'Submit'
+    expect(current_url).to have_content('Harry')
   end
 end
